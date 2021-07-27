267fe9a9eba2be9ca2a6113cf495218259f43409
ehsan1222 <s.e.maddahi.ir@gmail.com>
2021-07-27 22:46:50 +0430

    init project

---
+++ src/main/java/com/github/ehsan122/ca/CryptoAnalystApplication.java
@@ -0,0 +1,13 @@
+package com.github.ehsan122.ca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoAnalystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoAnalystApplication.class, args);
+	}
+
+}
