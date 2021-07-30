3f18379a07da4797428be12541ad780ae06fa9a0
ehsan1222 <s.e.maddahi.ir@gmail.com>
2021-07-30 21:53:42 +0430

    enable schedule

--- request/src/main/java/com/github/ehsan1222/ca/Application.java
+++ request/src/main/java/com/github/ehsan1222/ca/Application.java
@@ -2,8 +2,10 @@ package com.github.ehsan1222.ca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
