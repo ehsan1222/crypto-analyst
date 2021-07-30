4fb99d07a8249c7759697cbc043901d3507d7942
ehsan1222 <s.e.maddahi.ir@gmail.com>
2021-07-30 21:54:55 +0430

    add kafka produce config in request module

---
+++ request/src/main/java/com/github/ehsan1222/ca/config/KafkaProducerConfig.java
@@ -0,0 +1,39 @@
+package com.github.ehsan1222.ca.config;
+
+import com.binance.api.client.domain.market.Candlestick;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
+    @Bean
+    public ProducerFactory<String, List<Candlestick>> producerFactory() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(configs);
+    }
+
+    @Bean
+    public KafkaTemplate<String, List<Candlestick>> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+}
