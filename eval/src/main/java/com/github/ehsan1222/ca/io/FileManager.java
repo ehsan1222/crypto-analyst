5535f6f9de31936e659a4df43d546482f0bb3fcc
ehsan1222 <s.e.maddahi.ir@gmail.com>
2021-08-02 15:52:47 +0430

    add logger to fileManager

--- eval/src/main/java/com/github/ehsan1222/ca/io/FileManager.java
+++ eval/src/main/java/com/github/ehsan1222/ca/io/FileManager.java
@@ -1,29 +1,33 @@
 package com.github.ehsan1222.ca.io;
 
+import lombok.extern.java.Log;
+
 import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+@Log
 public class FileManager {
 
     public String read(Path path) {
         if (Files.notExists(path) || !Files.isReadable(path)) {
+            log.warning("path does not accessible " + path);
             return null;
         }
         try {
             return String.join("", Files.readAllLines(path));
         } catch (IOException e) {
+            log.warning("error while reading the file " + e.getMessage());
             return null;
         }
     }
 
     public String getMD5Hash(Path path) {
         if (Files.notExists(path) || !Files.isReadable(path)) {
+            log.warning("path does not accessible " + path);
             return "";
         }
         try {
@@ -31,9 +35,13 @@ public class FileManager {
             md.update(Files.readAllBytes(path));
             byte[] digest = md.digest();
             return DatatypeConverter.printHexBinary(digest).toUpperCase();
-        } catch (NoSuchAlgorithmException | IOException e) {
-            // TODO: logger
+        } catch (NoSuchAlgorithmException e) {
+            log.warning("invalid hash algorithm was specified " + e.getMessage());
+            return "";
+        } catch (IOException e) {
+            log.warning("error while hashing the file " + e.getMessage());
             return "";
         }
+
     }
 }
